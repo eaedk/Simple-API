77728d701dbae5dd6e48511d3ab92c9254150cb2
Emmanuel KOUPOH <emmanuelkoupoh@gmail.com>
2023-01-20 17:04:45 +0000

    code formatted

--- src/maths/api.py
+++ src/maths/api.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 import os, uvicorn
-# import joblib 
+
+# import joblib
 from typing import List, Literal
 from pydantic import BaseModel
 
@@ -17,9 +18,9 @@ ASSETS_DIRECTORY = os.path.join(CURRENT_FILE_DIRECTORY, "assets")
 
 # CONFIG
 app = FastAPI(
-    title="Math Tool",  
+    title="Math Tool",
     version="0.0.1",
-    description="This API with do simple math operations", 
+    description="This API with do simple math operations",
 )
 
 # API INPUT
@@ -33,78 +34,80 @@ class Input(BaseModel):
     customer_age : int
     gender : Literal['male', 'female', 'other']
     """
-    x : float
-    y : float
+
+    x: float
+    y: float
 
 
 # ENDPOINTS
 
 ## API STATUS
-@app.post("/status")  
+@app.post("/status")
 async def status():
-    return{"message": "online"}
-@app.get("/status")  
+    return {"message": "online"}
+
+
+@app.get("/status")
 async def status():
-    return{"message": "online"}
+    return {"message": "online"}
 
 
 ## Addition with structured input
-@app.post(
-    "/add" # 
-)  # the string in the post methode is the endpoint link
+@app.post("/add")  #  # the string in the post methode is the endpoint link
 async def operation(input: Input):
     "Function that receive the posted input data, do the operation and return an output/error message"
-    output = {} # None
+    output = {}  # None
 
     # try to execute the operation loop
     try:
         resutl = input.x + input.y
 
         # format output
-        output = {"result": resutl,
-                  "operation" :"addition",
-                  "way":"sending parameters as a payload",
-                  "x": input.x,
-                  "y": input.y}
+        output = {
+            "result": resutl,
+            "operation": "addition",
+            "way": "sending parameters as a payload",
+            "x": input.x,
+            "y": input.y,
+        }
 
     except ValueError as e:
         output = {"error": str(e)}
 
     except Exception as e:
         output = {"error": f"Oops something went wrong:\n{e}"}
-    
+
     finally:
         return output  # output must be json serializable
 
 
 ## Addition with input coming from the link/url
-@app.get(
-    "/add/{a}_{b}" # 
-)  # the string in the post methode is the endpoint link
-async def operation(a , b):
+@app.get("/add/{a}_{b}")  #  # the string in the post methode is the endpoint link
+async def operation(a, b):
     "Function that receive the posted input data, do the operation and return an output/error message"
-    output = {} # None
+    output = {}  # None
 
     # try to execute the operation loop
     try:
         resutl = a + b
 
         # format output
-        output = {"result": resutl,
-                  "operation" :"addition",
-                  "way":"Sending parameters by path/URL",
-                  "x": a,
-                  "y": b}
+        output = {
+            "result": resutl,
+            "operation": "addition",
+            "way": "Sending parameters by path/URL",
+            "x": a,
+            "y": b,
+        }
 
     except ValueError as e:
         output = {"error": str(e)}
 
     except Exception as e:
         output = {"error": f"Oops something went wrong:\n{e}"}
-    
+
     finally:
         return output  # output must be json serializable
-
 
 
 if __name__ == "__main__":
